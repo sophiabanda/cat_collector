361b0f71ad91de256a89038a2bad5e7ff3f9d451
Sophia M Banda <51414936+sophiabanda@users.noreply.github.com>
2024-04-19 16:57:48 -0400

    CATS page

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home),
-    path('about/', views.about)
+    path('about/', views.about),
+    path('cats/', views.cats_index),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
 
+#Temporary DB - Remove after adding cat model:
+cats = [
+    {'name': 'Lolo', 'breed': 'tabby', 'description': 'furry little demon', 'age': 3},
+    {'name': 'Sachi', 'breed': 'calico', 'description': 'gentle and loving', 'age': 2},
+]
+
 # Create your views here.
 
 def home(request):
@@ -7,3 +13,8 @@ def home(request):
 
 def about(request):
     return render(request, 'about.html')
+
+def cats_index(request):
+    return render(request, 'cats/index.html', {
+        'cats': cats
+    })
