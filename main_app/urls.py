291fef6869f028ffd297cc1ebb0363e051df2743
Sophia M Banda <51414936+sophiabanda@users.noreply.github.com>
2024-04-22 12:24:52 -0400

    updated template tags

--- main_app/urls.py
+++ main_app/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home),
-    path('about/', views.about),
-    path('cats/', views.cats_index),
+    path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
+    path('cats/', views.cats_index, name='index'),
 ]
